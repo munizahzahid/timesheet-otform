86c0edbde5bd0a0a5092e9cfb0f61737faa65848
Munizah Zahid <munizahzahid@gmail.com>
2026-06-08 16:24:06 +0800

    Change actual time inputs to text type for 24-hour format display (no AM/PM)

--- resources/views/ot-forms/partials/_executive_plan.blade.php
+++ resources/views/ot-forms/partials/_executive_plan.blade.php
@@ -153,8 +153,10 @@
                     {{-- ACTUAL START --}}
                     <td class="border border-gray-200 px-0.5 py-0.5 text-center">
                         @if($entry && $otForm->isEditable())
-                            <input type="time" name="entries[{{ $entry->id }}][actual_start_time]"
+                            <input type="text" name="entries[{{ $entry->id }}][actual_start_time]"
                                    value="{{ $entry->actual_start_time ? substr($entry->actual_start_time, 0, 5) : '' }}"
+                                   placeholder="HH:MM"
+                                   pattern="[0-9]{2}:[0-9]{2}"
                                    onchange="calcTotal({{ $entry->id }}, 'actual')"
                                    class="w-full border-0 text-xs py-1 px-0.5 text-center focus:ring-0 bg-transparent">
                         @elseif($entry)
@@ -165,8 +167,10 @@
                     {{-- ACTUAL END --}}
                     <td class="border border-gray-200 px-0.5 py-0.5 text-center">
                         @if($entry && $otForm->isEditable())
-                            <input type="time" name="entries[{{ $entry->id }}][actual_end_time]"
+                            <input type="text" name="entries[{{ $entry->id }}][actual_end_time]"
                                    value="{{ $entry->actual_end_time ? substr($entry->actual_end_time, 0, 5) : '' }}"
+                                   placeholder="HH:MM"
+                                   pattern="[0-9]{2}:[0-9]{2}"
                                    onchange="calcTotal({{ $entry->id }}, 'actual')"
                                    class="w-full border-0 text-xs py-1 px-0.5 text-center focus:ring-0 bg-transparent">
                         @elseif($entry)
--- resources/views/ot-forms/partials/_non_executive_plan.blade.php
+++ resources/views/ot-forms/partials/_non_executive_plan.blade.php
@@ -127,8 +127,10 @@
                     {{-- MASA SEBENAR: MULA --}}
                     <td class="border px-0.5 py-0.5 text-center">
                         @if($otForm->isEditable())
-                            <input type="time" name="entries[{{ $entry->id }}][actual_start_time]"
+                            <input type="text" name="entries[{{ $entry->id }}][actual_start_time]"
                                    value="{{ $entry->actual_start_time ? substr($entry->actual_start_time, 0, 5) : '' }}"
+                                   placeholder="HH:MM"
+                                   pattern="[0-9]{2}:[0-9]{2}"
                                    onchange="calcTotal({{ $entry->id }}, 'actual'); calcOT({{ $entry->id }})"
                                    class="w-full border-0 text-[10px] py-0 px-0 text-center focus:ring-0">
                         @else
@@ -139,8 +141,10 @@
                     {{-- MASA SEBENAR: TAMAT --}}
                     <td class="border px-0.5 py-0.5 text-center">
                         @if($otForm->isEditable())
-                            <input type="time" name="entries[{{ $entry->id }}][actual_end_time]"
+                            <input type="text" name="entries[{{ $entry->id }}][actual_end_time]"
                                    value="{{ $entry->actual_end_time ? substr($entry->actual_end_time, 0, 5) : '' }}"
+                                   placeholder="HH:MM"
+                                   pattern="[0-9]{2}:[0-9]{2}"
                                    onchange="calcTotal({{ $entry->id }}, 'actual'); calcOT({{ $entry->id }})"
                                    class="w-full border-0 text-[10px] py-0 px-0 text-center focus:ring-0">
                         @else
