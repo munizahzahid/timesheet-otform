2bcb48ceb1e975fee5583525ca8b0a7f088dfef1
munizahzahid <munizahzahid@gmail.com>
2026-07-28 09:23:42 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="icon" type="image/jpeg" href="{{ asset('images/Portal Logo.png') }}">
+        <link rel="icon" type="image/jpeg" href="{{ asset('images/TS.png') }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
