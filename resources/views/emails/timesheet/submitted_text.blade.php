f7ced432b863feea9ce729175a0c8e8d145c0ff6
Munizah Zahid <munizahzahid@gmail.com>
2026-08-10 12:04:55 +0800

    Simplify timesheet submission email to plain text without details or link

--- resources/views/emails/timesheet/submitted_text.blade.php
+++ resources/views/emails/timesheet/submitted_text.blade.php
@@ -1,15 +1,3 @@
 Timesheet Pending Approval
 
-Hello {{ $recipientName }},
-
-A timesheet has been submitted by {{ $staffName }} and is pending your approval.
-
-Staff: {{ $staffName }}
-Month / Year: {{ $monthYear }}
-Submitted At: {{ $submittedAt }}
-Status: {{ $statusLabel }}
-
-Please review the timesheet by visiting:
-{{ $link }}
-
-This is an automated message from {{ config('app.name') }}.
+Hello, you received new application.
