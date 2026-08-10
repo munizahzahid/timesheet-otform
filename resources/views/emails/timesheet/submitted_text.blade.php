e866e6621e39f9d127e8a068d483927e88fabba0
Munizah Zahid <munizahzahid@gmail.com>
2026-08-10 12:52:14 +0800

    Add staff and timesheet details to email without URL link

--- resources/views/emails/timesheet/submitted_text.blade.php
+++ resources/views/emails/timesheet/submitted_text.blade.php
@@ -1,1 +1,14 @@
-https://timesheet.62.238.2.126.sslip.io/
+Timesheet Pending Approval
+
+Hello {{ $recipientName }},
+
+A timesheet has been submitted by {{ $staffName }} and is pending your approval.
+
+Staff: {{ $staffName }}
+Month / Year: {{ $monthYear }}
+Submitted At: {{ $submittedAt }}
+Status: {{ $statusLabel }}
+
+Please log in to the system to review the timesheet.
+
+This is an automated message from {{ config('app.name') }}.
